9cea65ab1c1e0a81ad857c5f170328d544cdc130
Ezio Cangialosi <eziocangialosi@gmail.com>
2021-11-09 17:29:29 +0100

    Add array for properties data
    Format :
    -Identifier, Price, Rent
    
    Identifiers :
    0-"Free"
    1-Buy
    2-Start
    3-Chance
    4-Commu
    5-Prison
    6-Free space
    7-Go to prison

--- properties.h
+++ properties.h
@@ -2,10 +2,9 @@
 #define PROPERTIES_H
 #include <struct.h>
 
-const int estatePrice[22]={60,60,100,100,120,140,140,160,180,180,200,220,220,240,260,260,280,300,300,320,350,400};
 const int stationsPrice=200;
 const int companiesPrice=150;
-const int estateInitalRent[22]={2,4,6,6,8,10,10,12,14,14,16,18,18,20,22,22,22,26,26,28,35,50};
+
 const int stationsInitRent=25;
 const int taxesRent[2]={200,100};
 const string propNames[40]={"Start","Boulevard de Belleville","Caisse de Communauté","Rue Lecourbe","Impôts sur le revenu","Gare Montparnasse","Rue de Vaugirard","Chance","Boulevard Herteloup","Avenue de la République",
