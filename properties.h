f24983585139000da7412cb6bef5a0654b8db4f1
Ezio Cangialosi <eziocangialosi@gmail.com>
2021-11-20 09:22:51 +0100

    Change a property name
    From "Boulevard Herteloup" to "Boulevard Heurteloup"

--- properties.h
+++ properties.h
@@ -3,7 +3,7 @@
 #include <struct.h>
 
 //Array with properties Names
-const string propNames[40]={"Start","Boulevard de Belleville","Caisse de Communauté","Rue Lecourbe","Impôts sur le revenu","Gare Montparnasse","Rue de Vaugirard","Chance","Boulevard Herteloup","Avenue de la République",
+const string propNames[40]={"Start","Boulevard de Belleville","Caisse de Communauté","Rue Lecourbe","Impôts sur le revenu","Gare Montparnasse","Rue de Vaugirard","Chance","Boulevard Heurteloup","Avenue de la République",
                              "Prison (Simple Visite)","Boulevard de la Vilette","Compagnie d'Electricité","Avenue de Neuilly","Rue du Paradis","Gare de Lyon","Avenue Grammont","Caisse de Communauté",
                              "Boulevard Saint-Michel","Place Pigalle","Parc Gratuit","Avenue Matignon","Chance","Boulevard Malesherbes","Rue Nationale","Gare du Nord","Faubourg Saint-Honoré","Place de la Bourse","Compagnie des Eaux",
                              "Rue La Fayette","Allez en prison...","Avenue de Breteuil","Rue de Boulogne","Caisse de Communauté","Boulevard des Capucines","Gare Saint-Lazare","Chance","Avenue des Champs-Elysées",
